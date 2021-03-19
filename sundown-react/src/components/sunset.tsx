0c8c421fd210d79329985f95e72001e32024fe39
Jesper Hasselquist <jesper.hasselquist@gmail.com>
2021-03-20 01:10:56 +0900

    Fix ts issue

--- sundown-react/src/components/sunset.tsx
+++ sundown-react/src/components/sunset.tsx
@@ -9,7 +9,7 @@ function Sunset({ timeLeft }: SunsetProps) {
     const maxTimeDiff = 1000 * 60 * 60 * 24; // Max millis in 24 hours
     // maybe start the moving of the sun with a couple of hours left only
 
-    const sun = useRef(null);
+    let sun = useRef<HTMLDivElement>(null);
     useEffect(() => {
         if (sun !== null && sun.current !== null) {
             sun.current.style.top = `calc(50vh - (50vh * ${timeLeft/maxTimeDiff}))`;
